8a1ebc7fd871ecfcd3f4de370b0d7b32568f73d4
oldihoxha <oldi.hoxha@student.htw-berlin.de>
2025-04-08 14:14:47 +0200

    testen

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -7,6 +7,7 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
+    //testen
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
